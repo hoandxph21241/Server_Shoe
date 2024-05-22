9bced032616e1ee4bd145c885b8c5c0f7054249c
quoccon <quocnvph23753@fpt.edu.vn>
2024-05-22 14:02:52 +0700

    update ui edit product,add brand

--- BACK_END/Contronller/Manager_Controller.js
+++ BACK_END/Contronller/Manager_Controller.js
@@ -6,6 +6,10 @@ exports.AddProduct = async (req,res,next) => {
     res.render('manager/product/add_product.ejs');
 };
 
+exports.EditProduct = async (req,res,next) => {
+    res.render('manager/product/edit_product.ejs');
+};
+
 exports.VorcherList = async (req,res,next) => {
     res.render('manager/vorcher/vorcher.ejs');
 };
--- BACK_END/routes/Manager_Router.js
+++ BACK_END/routes/Manager_Router.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var ManagerController = require('../Contronller/Manager_Controller');
 
 router.get('/productlist',ManagerController.ProductList);
+router.get('/edit-product',ManagerController.EditProduct);
+
+
 router.get('/addproduct',ManagerController.AddProduct);
 router.get('/vorcherlist',ManagerController.VorcherList);
 
