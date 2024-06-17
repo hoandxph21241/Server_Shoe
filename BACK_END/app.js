c1cde607c8677cf9225ab588607ffc5396cec7d3
phuccvph14072003 <130788105+thanhanDemon2003@users.noreply.github.com>
2024-06-17 21:40:02 +0700

    Update app.js

--- BACK_END/app.js
+++ BACK_END/app.js
@@ -6,7 +6,7 @@ var logger = require("morgan");
 var app = express();
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb+srv://vanphuc_mongodb:1E7V8mnJNV4X9ead@cluster0.iit1hgd.mongodb.net/Shoe")
+  .connect("mongodb+srv://hoandxph21241:hoan123@shoesdatabase.ck50y7r.mongodb.net/Shoes_Database")
   .then(() => console.log('>>>>>>>>>> DB Connected!!!!!!'))
   .catch(err => console.log('>>>>>>>>> DB Error: ', err));
 
